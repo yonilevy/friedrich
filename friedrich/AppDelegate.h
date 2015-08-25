b0bfa3a06f8b5e181736c544de854c4257e69b58
yonilevy <yonilevy@gmail.com>
2015-08-25 21:37:51 +0300

    clean some xcode generated crap

--- friedrich/AppDelegate.h
+++ friedrich/AppDelegate.h
@@ -1,17 +1,7 @@
-//
-//  AppDelegate.h
-//  friedrich
-//
-//  Created by Yoni Levy on 8/25/15.
-//  Copyright (c) 2015 Yoni Levy. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
-
+@property(strong, nonatomic) UIWindow *window;
 
 @end
-
--- friedrich/ViewController.m
+++ friedrich/ViewController.m
@@ -1,11 +1,3 @@
-//
-//  ViewController.m
-//  friedrich
-//
-//  Created by Yoni Levy on 8/25/15.
-//  Copyright (c) 2015 Yoni Levy. All rights reserved.
-//
-
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -14,14 +6,9 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
